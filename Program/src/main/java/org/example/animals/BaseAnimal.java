012eeaab7fec70c5e525bdbd13c7fef0f4d3eacb
Margarita Kharokhorina <kharokhorina_mb@mail.ru>
2023-09-28 13:24:44 +0300

    Add base class

--- Program/src/main/java/org/example/animals/BaseAnimal.java
+++ Program/src/main/java/org/example/animals/BaseAnimal.java
@@ -1,5 +1,23 @@
 package main.java.org.example.animals;
 
 public class BaseAnimal {
-    
+    public String name; // Имя животного
+    public String mainClass; // Класс животного
+    public String nameClass; // Подкласс животного
+    public String birthday; // Дата рождения
+
+    public String[] skills; // Умения
+
+    public BaseAnimal (String name, String mainClass, String nameClass, String birthday, String[] skills) {
+        this.name = name;
+        this.mainClass = mainClass;
+        this.nameClass = nameClass;
+        this.birthday = birthday;
+        this.skills = skills;
+    }
+
+    public String getInfo() {
+        String str = mainClass + " " + nameClass + " " + name + " " + birthday + " " + String.join(" / ",skills);
+        return str;
+    }
 }
